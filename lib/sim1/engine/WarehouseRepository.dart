dd7769fac624005c9401d60276fc6a069d09bbf5
劉達 <ryu@anchorz.co.jp>
2021-09-11 20:53:13 +0900

    bk

--- lib/sim1/engine/WarehouseRepository.dart
+++ lib/sim1/engine/WarehouseRepository.dart
@@ -1,3 +1,6 @@
+import 'package:business_sim_a/sim1/engine/EngineCenter.dart';
+import 'package:business_sim_a/sim1/engine/GameObj.dart';
+
 class WarehouseRepository {
   int _warehouseCount = 0;
   int getWarehouseCount() => _warehouseCount;
@@ -5,8 +8,33 @@ class WarehouseRepository {
 
 class Warehouse {
   // 使用期限
-  int _expiry;
+  late int expiryTurn;
+  GameObj? _obj;
 
   /// 不注明使用期限时表示无限期
-  Warehouse({this._expiry = 0});
+  Warehouse({int expiry = 0}) {
+    expiryTurn = EngineCenter.shared.getTurn() + expiry;
+  }
+
+  void insertObj(GameObj newObj) {
+    if (_obj == null) {
+      _obj = newObj;
+    } else {
+      throw Error();
+    }
+  }
+
+  GameObj takeObj() {
+    if (_obj != null) {
+      final _saveObj = _obj;
+      _obj = null;
+      return _saveObj!;
+    } else {
+      throw Error();
+    }
+  }
+
+  GameObj? checkObj() {
+    return _obj;
+  }
 }
